e6dbf5f190427972d0e3204694fb877dc264cd37
AndreyTarkaniy <endrubass@gmail.com>
2023-02-25 22:35:19 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -7,10 +7,12 @@ console.log(Number.parseInt(inputEl.dataset.length))
 inputEl.addEventListener("blur", () => {
 
     if( inputEl.value.length === Number.parseInt(inputEl.dataset.length) ){ 
-        inputEl.classList.add("valid")   
+        inputEl.classList.add("valid")
+        inputEl.classList.remove("invalid")       
   
     }else {        
-        inputEl.classList.add("invalid")            
+        inputEl.classList.add("invalid") 
+        inputEl.classList.remove("valid")                  
     }
  
     console.log(inputEl.value.length)
